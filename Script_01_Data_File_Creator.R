5d51c435992a9618e184e9be4d4c26a2a85fc106
PatMaier92 <maier_patrizia@web.de>
2021-10-06 13:45:37 +0200

    Update all cleaning steps

--- Script_01_Data_File_Creator.R
+++ Script_01_Data_File_Creator.R
@@ -20,78 +20,74 @@ path <- "WP6_data/"
 sm_file <- paste(path, "WP6_result_table_211005.xlsx", sep="")
 sm_data <- read_xlsx(sm_file, sheet = "sm6", col_names = T)
 participants <- unique(sm_data$id) # final sample of included participants
+rm(sm_file)
 
 # starmaze motor control 
-mc_file <- paste(path, "WP6_SM_mct_table_211006.xlsx", sep="")
-mc_data <- read_xlsx(mc_file, sheet = "SM_mct", col_names = T)
+motor_control_file <- paste(path, "WP6_SM_mct_table_211006.xlsx", sep="")
+motor_control_data <- read_xlsx(motor_control_file, sheet = "SM_mct", col_names = T)
+rm(motor_control_file)
 
 
 # drawing score
 score_file <- paste(path, "WP6_RecallRecognition_scoring_210811.xlsx", sep="")
 score_data <- read_xlsx(score_file, sheet = "WP6_all", col_names=T, na = "NA")
 score_data <- score_data[score_data$ID %in% participants, ] # remove excluded participants
+rm(score_file)
 
 # gmda scores
 gmda_draw_file <-  paste(path, "WP6_GMDA_data_drawing_210811.Rdata", sep="")
 load(gmda_draw_file)
-gmda_draw <- temp
+gmda_draw <- temp[temp$ID %in% participants, ] # remove excluded participants
 rm(temp, gmda_draw_file)
-gmda_draw <- gmda_draw[gmda_draw$ID %in% participants, ] # remove excluded participants
 
 gmda_recog_file <-  paste(path, "WP6_GMDA_data_recognition_210811.Rdata", sep="")
 load(gmda_recog_file)
-gmda_recog <- temp
+gmda_recog <- temp[temp$ID %in% participants, ] # remove excluded participants
 rm(temp, gmda_recog_file) 
-gmda_recog <- gmda_recog[gmda_recog$ID %in% participants, ] # remove excluded participants
 
 
 # neuropsychology
 np_file <- paste(path, "Auswertung WP06_6200-6300_CLEANED_210805.sav", sep="") 
 np_data <- read.spss(np_file, use.value.labels=T, to.data.frame=T)
 np_data <- np_data[np_data$info_id %in% participants, ] # remove excluded participants
+rm(np_file)
 
 
 # clinical data
 clin_file <- paste(path, "WP6_data_clinical_data_ALS_210602.xlsx", sep="")
 clin_data <- read_xlsx(clin_file, sheet = "Tabelle1", col_names=T)
 clin_data <- clin_data[clin_data$ID %in% participants, ] # remove excluded participants
+rm(clin_file)
 
 
 # load ecas norm values
-ecas_norms_2015 <- read_xlsx("Normen_ECAS_Lule_2015.xlsx", sheet="Tabelle1", col_names=T)
 ecas_norms_2016 <- read_xlsx("Normen_ECAS_Loose_2016.xlsx", sheet="Tabelle1", col_names=T)
 
 ###############################################################################
 
 
-# # STARMAZE TRIAL DATA
-# # select data
-# int_vars <- c("ID", "Group", "Trial", "Trial_Condition", "Feedback","Start_position",
-#               "success", "time_abs", "time_accuracy", "path_abs", "path_accuracy","distance_accuracy", 
-#               "final_distance_to_allo_abs", "final_distance_to_ego_abs","Path_score","Direct_Path",
-#               "head_rotation_abs", "head_turn_abs") 
-# sm_data <- sm_data[,int_vars]
-# rm(int_vars)
-
-# # correction of errors 
-# # 1) for subjects who saw video (= 32 instead of 31 trials)
-# # Trial: subtract -1 from all trial numbers > 4
-# participants_video <- sm_data$ID[sm_data$Trial==32]
-# sm_data$Trial[sm_data$ID %in% participants_video & sm_data$Trial > 4] <- sm_data$Trial[sm_data$ID %in% participants_video & sm_data$Trial > 4]-1
-# # Feedback: correct order
-# correct_feedback <- sm_data$Feedback[sm_data$ID == 6201]
-# for (id in participants_video){
-#   sm_data$Feedback[sm_data$ID == id] <- correct_feedback
-# }
-# # 2) general error 
-# sm_data$Feedback[sm_data$Trial == 26] <- 1
+## STARMAZE TRIAL DATA
+trial_data <- sm_data %>% 
+  select(!c(wp, group_no, trial_type)) %>% 
+  rename(group=group_name, probe_trial=feedback) %>% 
+  mutate(group=fct_recode(group, MND="MNE", Control="Control"),
+         trial_condition=fct_recode(factor(trial_condition), training="0", allocentric="1", egocentric="2", mixed="3"),
+         probe_trial=case_when(probe_trial==0 ~ 1,
+                               probe_trial==1 ~ 0))
+rm(sm_data)
 
 
+## STARMAZE MOTOR CONTROL DATA 
+mc_data <- motor_control_data %>% 
+  select(!c(wp, group_no, mct_sumBodyRotation, mct_sumHeadRotation, mct_bodyRotation, mct_headRotation)) %>% 
+  mutate(group=fct_recode(group, MND="MNE", Control="Control")) %>% 
+  rename(ID=id)
+rm(motor_control_data)
+  
 
 ## SCORING DATA 
 # clean data 
 sc_data <- score_data %>% 
-  filter(!is.na(ID)) %>% 
   select(!Maze_reconstruction...12 & !Group) %>% 
   rename(old_Score_total_manual=Score_total,
          old_Object_location_manual=Object_location,
@@ -102,11 +98,9 @@ sc_data <- score_data %>%
          Object_identity_manual=Object_Identity,
          Object_identity_manual_drawing=Object_Identity_recall,
          Object_identity_manual_recognition=Object_Identity_recognition) %>% 
-  mutate(Maze_reconstruction_manual_z=Maze_reconstruction_manual / 16,
-         Object_identity_manual_z=Object_identity_manual / 28)
-
-# remove excluded observations 
-sc_data <- sc_data[sc_data$ID %in% participants, ]
+  mutate(Maze_reconstruction_manual_s=Maze_reconstruction_manual / 16,
+         Object_identity_manual_s=Object_identity_manual / 28)
+rm(score_data)
 
 # add gmda data 
 g_d <- gmda_draw %>% 
@@ -126,9 +120,9 @@ g_r <- gmda_recog %>%
 sc_data <- sc_data %>% 
   left_join(g_d) %>% 
   left_join(g_r) %>% 
-  mutate(Object_location_GMDA_SQRTCanOrg_z=(GMDA_SQRTCanOrg_drawing + GMDA_SQRTCanOrg_recognition)/2,
-         Score_total=(Maze_reconstruction_manual_z + Object_identity_manual_z + Object_location_GMDA_SQRTCanOrg_z)/3)
-rm(g_r, g_d)
+  mutate(Object_location_GMDA_SQRTCanOrg_s=(GMDA_SQRTCanOrg_drawing + GMDA_SQRTCanOrg_recognition)/2,
+         Score_total=(Maze_reconstruction_manual_s + Object_identity_manual_s + Object_location_GMDA_SQRTCanOrg_s)/3)
+rm(g_r, g_d, gmda_draw, gmda_recog)
 
 
 ## CLINICAL DATA
@@ -139,7 +133,7 @@ c_data <- clin_data %>%
          Körpergröße, KG, BMI, cs_bulbar, cs_pseudobulbar, cs_dysarthria,cs_dysphagia,
          cs_monoparesis, cs_paraparesis, cs_tetraparesis, cs_myatrophy, cs_fasciculation, 
          cs_fibrillation, cs_spasticity, cs_hyperreflexy, cs_motoricDisinihibition, cs_gaitDisorder,
-         med_hypoventilation, med_Riluzol, `med:Cannabis`, med_Baclofen,
+         med_hypoventilation, med_Riluzol, `med_Cannabis`, med_Baclofen,
          med_siallorhoe, med_cns, med_depression, med_anxiety, med_other_1, med_other_2,
          med_other_3, medCon_additional_1, medCon_additional_2, medCon_additional_3,
          initial_symptome_timepoint, initial_symptom, is_region, is_lateralized, is_category, is_t1_months,
@@ -150,31 +144,30 @@ c_data <- clin_data %>%
          ALS_Variante = factor(ALS_Variante, labels=c("ALS-Bulbär", "ALS", "ALS-spastisch", "ALS-pereoneal")),
          is_category = factor(is_category, labels=c("bulbär", "parese", "spastik")),
          MN=factor(MN, labels=c("1. MN", "2. MN", "1. und 2. MN")))
-  
+rm(clin_data)  
+
 # add dummy observations for controls  
 controls <- participants[!participants %in% c_data$ID]
 c_data <- c_data %>% 
   add_row(ID=controls)
 
-# remove excluded observations
-c_data <- c_data[c_data$ID %in% participants, ]
-
-
 
 ## NEUROPSYCHOLOGY
 # clean data
 n_data <- np_data %>% 
-  select(!c("info_t1_star_start","info_t1_star_end","info_t1_starmaze_notes",
-            "ECAS_start","ECAS_end","ECAS_fam_1degree","ECAS_fam_2degree",
-            "ECAS_first_symptoms","ECAS_first_diagnosis","ECAS_not_invasive_resp",
-            "ECAS_status_beginn_bulbuar","ECAS_status_beginn_lower","ECAS_status_beginn_upper",
-            "ECAS_status_now_bulbuar","ECAS_status_now_lower","ECAS_status_now_upper",
-            "ECAS_height","ECAS_weight","ECAS_PEG","INFO_0","info_date",
+  select(!c(info_group, info_t1_star_start, info_t1_star_end, info_t1_starmaze_notes,
+            ECAS_start, ECAS_end, ECAS_fam_1degree, ECAS_fam_2degree,
+            ECAS_first_symptoms, ECAS_first_diagnosis, ECAS_not_invasive_resp,
+            ECAS_status_beginn_bulbuar, ECAS_status_beginn_lower, ECAS_status_beginn_upper,
+            ECAS_status_now_bulbuar, ECAS_status_now_lower, ECAS_status_now_upper,
+            ECAS_height, ECAS_weight, ECAS_PEG,INFO_0, info_date
             )) %>% 
-  rename(ID=info_id, Group=info_group) 
-
-# remove excluded observations
-n_data <- n_data[n_data$ID %in% participants, ]
+  mutate(dfb_q1_sex=fct_recode(dfb_q1_sex, male="männlich", female="weiblich"),
+         dfb_q4_highestedu=factor(dfb_q4_highestedu),
+         dfb_q5_language_german=factor(dfb_q5_language_german),
+         dfb_q6_handiness=factor(dfb_q6_handiness)) %>% 
+  rename(ID=info_id)   
+rm(np_data)
 
 # add cut-off values 
 cut_off_func <- function(age, isced_school_II, norm_cat){
@@ -185,13 +178,6 @@ cut_off_func <- function(age, isced_school_II, norm_cat){
     age < 60 & isced_school_II > 12 ~ ecas_norms_2016$cut_under_60_over_12[ecas_norms_2016$measures==norm_cat],
     age >= 60 & isced_school_II <= 12 ~ ecas_norms_2016$cut_overequal_60_underequal_12[ecas_norms_2016$measures==norm_cat],
     age >= 60 & isced_school_II > 12 ~ ecas_norms_2016$cut_overequal_60_over_12[ecas_norms_2016$measures==norm_cat]
-    
-    # old norms from Lulé 2015
-    # age <= 60 & isced_school_II < 12 ~ ecas_norms_2015$cut_underequal_60_under_12[ecas_norms_2015$measures==norm_cat],
-    # age > 60 & isced_school_II < 12 ~ ecas_norms_2015$cut_over_60_under_12[ecas_norms_2015$measures==norm_cat],
-    # age <= 60 & isced_school_II >= 12 ~ ecas_norms_2015$cut_underequal_60_overequal_12[ecas_norms_2015$measures==norm_cat],
-    # age > 60 & isced_school_II >= 12 ~ ecas_norms_2015$cut_over_60_overequal_12[ecas_norms_2015$measures==norm_cat]
-    # 
     )
   
   return(my_cut_off)
@@ -219,41 +205,30 @@ n_data <- n_data %>%
     ECAS_sub_spatial_cut = cut_off_func(dfb_q2_age, dfb_q3_years_school_clean, "Visuospatial"),
     ECAS_sub_spatial_below_cut = ECAS_sub_spatial < ECAS_sub_spatial_cut
   )
+rm(ecas_norms_2016)
 
 
-## create data frame with CLINICAL, NEUROPSYCHOLOGY AND SCORE DATA
-# data <- sm_data %>% 
-#   full_join(sc_data) %>% 
-#   full_join(c_data) %>% 
-#   full_join(n_data)
-
-data_individual <- n_data %>% 
+## create data frame with MOTOR CONTROL, CLINICAL, NEUROPSYCHOLOGY AND SCORE DATA
+data_individual <- mc_data %>% 
+  full_join(n_data) %>% 
   full_join(c_data) %>% 
   full_join(sc_data)
-
-rm(score_data, np_data, clin_data)
-
-data_individual <- data_individual %>% 
-  mutate(Group=fct_recode(Group, MNE="Experimental", Control="Controll"),
-         dfb_q1_sex=fct_recode(dfb_q1_sex, male="männlich", female="weiblich"),
-         dfb_q4_highestedu=factor(dfb_q4_highestedu),
-         dfb_q5_language_german=factor(dfb_q5_language_german),
-         dfb_q6_handiness=factor(dfb_q6_handiness))
+rm(mc_data, n_data, c_data, sc_data)
 
 
 #############################################################################
 
 
 ## save data as Rdata 
-out_fileR <-  paste(path, "WP6_data_", date, ".Rdata", sep="")
+out_fileR <-  paste(path, "WP6_individual_data.Rdata", sep="")
 save(data_individual, file=out_fileR)
 
-# out_fileTR <-  paste(path, "WP6_trial_data_", date, ".Rdata", sep="")
-# save(data_trial), file=out_fileTR)
+out_fileTR <-  paste(path, "WP6_trial_data.Rdata", sep="")
+save(trial_data, file=out_fileTR)
 
 
 ## save data as excel 
-out_fileXLSX <-  paste(path, "WP6_data_", date, ".xlsx", sep="")
+out_fileXLSX <-  paste(path, "WP6_individual_data.xlsx", sep="")
 
 wb <- createWorkbook()
 addWorksheet(wb, "Data_individual")
@@ -266,8 +241,11 @@ rm(wb)
 
 
 ## save data as csv (for JASP)
-out_fileCSV<-  paste(path, "WP6_data_", date, ".csv", sep="")
-write.csv(data_individual,file=out_fileCSV, row.names=FALSE)
+out_fileCSV <-  paste(path, "WP6_individual_data.csv", sep="")
+write.csv(data_individual, file=out_fileCSV, row.names=FALSE)
+
+out_fileCSVTR <-  paste(path, "WP6_trial_data.csv", sep="")
+write.csv(trial_data, file=out_fileCSVTR, row.names=FALSE)
 
 
 ## clear workspace
